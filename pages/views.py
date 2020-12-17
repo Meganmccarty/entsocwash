edbcd378c8e1f0841edc424b9f4893b95ba02088
Megan McCarty <megan_mccarty@hotmail.com>
2020-12-17 15:44:46 -0500

    Views for upcoming/past meetings

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.utils import timezone
+from django.views import generic
+from .models import Meeting
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
-# Create your views here.
+class UpcomingMeetingsListView(generic.ListView):
+    model = Meeting
+    template_name = 'pages/upcoming_meetings.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(UpcomingMeetingsListView, self).get_context_data(**kwargs)
+        context['upcoming_meetings'] = Meeting.objects.filter(date__gte=timezone.now()).order_by('-date')
+        return context
+
+class PastMeetingsListView(generic.ListView):
+    model = Meeting
+    template_name = 'pages/past_meetings.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(PastMeetingsListView, self).get_context_data(**kwargs)
+        context['past_meetings'] = Meeting.objects.filter(date__lte=timezone.now()).order_by('-date')
+        return context
