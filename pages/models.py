edbc66684bac75d56bb3646c2eebc85e5096731f
Megan McCarty <megan_mccarty@hotmail.com>
2020-12-17 15:43:45 -0500

    Modified Event/Meeting model, updated HomePage

--- pages/models.py
+++ pages/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.utils import timezone
 
 from wagtail.core.models import Page
 from wagtail.core.fields import StreamField, RichTextField
@@ -10,12 +11,11 @@ from wagtail.images.blocks import ImageChooserBlock
 
 class HomePage(Page):
     body = StreamField([
-        ('subtitle', blocks.CharBlock(classname="full title")),
+        ('subtitle', blocks.CharBlock(classname="full title", help_text="Enter a subtitle for the page")),
         ('section', blocks.StructBlock([
-            ('heading', blocks.CharBlock()),
-            ('paragraph', blocks.RichTextBlock()),
+            ('heading', blocks.CharBlock(help_text="Enter a heading for a section of text on the page")),
+            ('text', blocks.RichTextBlock()),
             ('image', ImageChooserBlock(required=False)),
-            ('more_information', PageChooserBlock(required=False, help_text="Select a page where a member can find more information")),
         ])),
     ])
     
@@ -26,16 +26,8 @@ class HomePage(Page):
     def get_context(self, request):
         # Update context to include only published events, ordered by reverse-chron
         context = super().get_context(request)
-        eventspages = self.get_children().live().order_by('-first_published_at')
-        paginator = Paginator(eventspages, 2)
-        page = request.GET.get("page")
-        try:
-            eventspages = paginator.page(page)
-        except PageNotAnInteger:
-            eventspages = paginator.page(1)
-        except EmptyPage:
-            eventspages = paginator.page(paginator.num_pages)
-        context['eventspages'] = eventspages
+        upcoming_meetings = Meeting.objects.filter(date__gte=timezone.now()).order_by('-date')
+        context['upcoming_meetings'] = upcoming_meetings
         return context
 
 class GenericPage(Page):
@@ -53,18 +45,14 @@ class GenericPage(Page):
         StreamFieldPanel('body'),
     ]
 
-class EventPage(Page):
-    body = StreamField([
-        ('subtitle', blocks.CharBlock(classname="full title")),
-        ('section', blocks.StructBlock([
-            ('heading', blocks.CharBlock()),
-            ('date', blocks.DateTimeBlock()),
-            ('location', blocks.RichTextBlock()),
-            ('paragraph', blocks.RichTextBlock()),
-            ('image', ImageChooserBlock(required=False)),
-        ])),
-    ])
-    
-    content_panels = Page.content_panels + [
-        StreamFieldPanel('body'),
-    ]
+class Meeting(models.Model):
+    title = models.CharField(default='', max_length=100, help_text='Add a title for this specific meeting.')
+    date = models.DateTimeField(help_text='Enter the date and time of the meeting.')
+    meeting_details = models.TextField(default='', help_text='Include the specifics of the meeting')
+    image = models.ImageField(null=True, blank=True, help_text='Optional; select an image to go with this meeting.')
+
+    class Meta:
+        ordering = ['-date']
+
+    def __str__(self):
+        return self.title
