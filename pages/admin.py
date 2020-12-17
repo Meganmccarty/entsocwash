a300df14c5a380cadc9b8e378a316c5ffdffe3a8
Megan McCarty <megan_mccarty@hotmail.com>
2020-12-17 15:44:28 -0500

    Added Meeting to wagtail cms menu

--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,18 @@
-from django.contrib import admin
+from wagtail.contrib.modeladmin.options import (
+    ModelAdmin,
+    modeladmin_register,
+)
 
-# Register your models here.
+from .models import Meeting
+
+class MeetingAdmin(ModelAdmin):
+    model = Meeting
+    menu_label = "Meetings"
+    menu_icon = "pick"
+    menu_order = 290
+    add_to_settings_menu = False
+    exclude_from_explorer = False
+    list_display = ("title", "date", "meeting_details")
+    search_fields = ("title", "date", "meeting_details")
+
+modeladmin_register(MeetingAdmin)
